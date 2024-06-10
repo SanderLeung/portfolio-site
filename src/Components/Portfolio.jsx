ce5f7f103bb2f6ecfd163c45f87db4a42c427a49
Sander Leung <sander.leung@gmail.com>
2024-06-10 19:36:11 +0000

    Personalization update

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/robot-working.png";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "robot studying at his desk";
 
 /**
  * Project list
@@ -31,28 +31,27 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Protein Calculator",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "Originally created for The Odin Project. Allows users to add food items and calculate protein efficiency.",
+    url: "https://github.com/SanderLeung/protein-calculator",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Nutrition Visualizer in Streamlit",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "Using Streamlit, this project visualizes nutrition data from the protein calculator database.",
+    url: "https://github.com/SanderLeung/streamlit-project",
+  },
+  {
+    title: "Portfolio Site",
+    description: "Created from Microsoft's template repo and deployed to GitHub pages.",
+    url: "https://github.com/SanderLeung/portfolio-site",
   },
   {
     title: "My Resume Site",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
-  },
-  {
-    title: "GitHub Codespaces and github.dev",
-    description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "Ask quesions using an LLM against a PostgreSQL database utilizing RAG hybrid search.",
+    url: "https://github.com/SanderLeung/rag-postgres-llm-python",
   },
 ];
 
